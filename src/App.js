b33663f0be13f0cbd9fb621a46e98173d94bb48b
okothpius <orukopius@gmail.com>
2021-04-16 13:28:17 +0300

    feat: import header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './components/ui/Header';
 import './App.css';
 
 const App = () => {
-  return <div className="container">Hello</div>
+  return (
+    <div className='container'>
+      <Header />
+    </div>
+  )
 }
 
 export default App;
